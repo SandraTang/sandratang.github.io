7ec8cef56e0d1c50713affd707eb2b15c7da860b
Sandra Tang <sandratang01@gmail.com>
2024-12-04 22:18:23 -0800

    scroll controlled logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,27 +1,35 @@
 import { useGLTF } from "@react-three/drei";
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 import * as THREE from "three";
-import React from "react";
 
 function Logo() {
   const logoRef = useRef<THREE.Object3D>(null);
   const { scene } = useGLTF("/logo.glb");
   const scale = 0.025;
-  const [x, setX] = React.useState(0);
+  const [scrollY, setScrollY] = useState(0);
 
-  React.useEffect(() => {
-    const interval = setInterval(() => {
-      setX((x) => x + 0.015);
-    }, 10);
-    return () => clearInterval(interval);
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
+
+  // Map scrollY to a rotation range, e.g., 0 to 2 * Math.PI
+  const rotationX = 1 - Math.cos(scrollY * 0.005);
+  const rotationY = Math.PI + Math.sin(scrollY * 0.005);
 
   return (
     <primitive
       ref={logoRef}
       object={scene.clone()}
       scale={[scale, scale, scale]}
-      rotation={[0.3 * Math.cos(x), Math.PI + 0.3 * Math.sin(x), 0]}
+      rotation={[rotationX, rotationY, 0]}
     />
   );
 }
