5e74f75cc38ad8723214a8b5e0ce92f436abbf7e
Sandra Tang <sandratang01@gmail.com>
2026-07-02 14:07:54 -0700

    landing logo movement

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -7,22 +7,35 @@ function Logo() {
   const { scene } = useGLTF("/logo.glb");
   const scale = 0.025;
   const [scrollY, setScrollY] = useState(0);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     const handleScroll = () => {
       setScrollY(window.scrollY);
     };
 
+    const handleMouseMove = (event: MouseEvent) => {
+      const normalizedX = event.clientX / window.innerWidth - 0.5;
+      const normalizedY = event.clientY / window.innerHeight - 0.5;
+
+      setMousePosition({ x: normalizedX, y: normalizedY });
+    };
+
     window.addEventListener("scroll", handleScroll);
+    window.addEventListener("mousemove", handleMouseMove);
 
     return () => {
       window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("mousemove", handleMouseMove);
     };
   }, []);
 
-  // Map scrollY to a rotation range, e.g., 0 to 2 * Math.PI
-  const rotationX = 1 - Math.cos(scrollY * 0.005);
-  const rotationY = Math.PI + Math.sin(scrollY * 0.005);
+  const scrollRotationX = 1.5 * (1 - Math.cos(scrollY * 0.005));
+  const scrollRotationY = Math.PI + Math.sin(scrollY * 0.005);
+  const mouseRotationX = mousePosition.y * 1.2;
+  const mouseRotationY = mousePosition.x * 1.2;
+  const rotationX = scrollRotationX + mouseRotationX;
+  const rotationY = scrollRotationY + mouseRotationY;
 
   return (
     <primitive
